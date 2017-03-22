6ec85502a6231f46effd1d557d23e31ef5c04e31
rafvillamor <raf.villamor@yahoo.com>
2017-03-22 15:01:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
  var qty = document.getElementById('qty').value; 
  var new_qty = parseInt(qty,10) + val; 
       
-      if (new_qty < 0) { 
-          new_qty = 0; 
+      if (new_qty < 10) { 
+          new_qty = 10; 
           } 
       
      document.getElementById('qty').value = new_qty; 
